2494f963bf0db4f8e91ba3f3aee408878c749468
Martijn Wesselius <martijn.wesselius@gmail.com>
2022-05-26 23:47:05 +0200

    add new color scheme, animation and a color picker

--- Etch-A-Sketch_files/script.js
+++ Etch-A-Sketch_files/script.js
@@ -1,13 +1,13 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = "#404040";
-const DEFAULT_MODE = "color";
+const DEFAULT_MODE = "black";
 
 let currentSize = DEFAULT_SIZE;
 let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 
-const colorPicker = document.querySelector("#color-picker");
-const blackButton = document.querySelector("#color-button");
+const colorPicker = docuemtn.querySelector("#color-picker");
+const blackButton = document.querySelector("#black-button");
 const rainbowButton = document.querySelector("#rainbow-button");
 const eraserButton = document.querySelector("#eraser-button");
 const clearButton = document.querySelector("#clear-button");
@@ -16,17 +16,16 @@ const slider = document.querySelector(".slider");
 const grid = document.querySelector("#grid");
 
 colorPicker.oninput = (e) => setCurrentColor(e.target.value); 
-blackButton.onclick = () => setCurrentMode("color");
+blackButton.onclick = () => setCurrentMode("black");
 rainbowButton.onclick = () => setCurrentMode("rainbow");
 eraserButton.onclick = () => setCurrentMode("eraser");
 clearButton.onclick = () => reloadGrid();
 slider.oninput = (e) => updateSliderValue(e.target.value);
 slider.onchange = (e) => changeSizeGrid(e.target.value);
 
-let mouseDown = false;
+let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
-
 
 
 function setCurrentSize(newSize) {
@@ -41,6 +40,7 @@ function setCurrentMode(newMode) {
     activateButton(newMode);
     currentMode = newMode;
 }
+
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -58,7 +58,7 @@ function setupGrid(size) {
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return
     switch (currentMode) {
-        case "color":
+        case "black":
             e.target.style.backgroundColor = currentColor;
             break;
         case "rainbow":
@@ -69,6 +69,7 @@ function changeColor(e) {
             break;
     }
 }
+
 
 function reloadGrid() {
     grid.innerHTML = "";
@@ -89,7 +90,7 @@ function getRandomColor() {
 }
 
 function activateButton(newMode) {
-    if (currentMode === "color") {
+    if (currentMode === "black") {
         blackButton.classList.remove("active");
     } else if (currentMode === "rainbow") {
         rainbowButton.classList.remove("active");
@@ -97,7 +98,7 @@ function activateButton(newMode) {
         eraserButton.classList.remove("active");
     }
 
-    if (newMode === "color") {
+    if (newMode === "black") {
         blackButton.classList.add("active");
     } else if (newMode === "rainbow") {
         rainbowButton.classList.add("active");
@@ -108,3 +109,4 @@ function activateButton(newMode) {
 
 setupGrid(DEFAULT_SIZE);
 activateButton(DEFAULT_MODE);
+setCurrentColor(DEFAULT_COLOR);
